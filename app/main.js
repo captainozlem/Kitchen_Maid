8a0d2eaaec367c3727a37167af4e340d581c5ed6
captainozlem <ozlemniksch@gmail.com>
2019-01-02 18:15:19 -0500

    it bundled but there is a problem, you can see the message in the bundle.js file

--- app/main.js
+++ app/main.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {render} from 'react-dom';
+import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {BrowserRouter as Router} from 'react-router-dom';
 import store from './store';
@@ -7,7 +7,7 @@ import Root from './components/Root';
 import {Navbar} from './components/Navbar';
 import '../public/index.css';
 
-render(
+ReactDOM.render(
   <Provider store={store}>
     <Router>
       <Root>
